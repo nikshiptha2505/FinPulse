ce96a67530e946d42e9653c1d22156f46d789d6f
Nikshiptha Penthala <nikshipthapenthala@gmail.com>
2026-03-31 14:46:39 +0530

    updated frontend API URL

--- FinPulse-Frontend/vite.config.js
+++ FinPulse-Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://finpulse-backend-7h89.onrender.com',
         changeOrigin: true,
       },
     },
